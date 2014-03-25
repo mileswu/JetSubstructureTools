463eca0bc757a7c25030733f88ebeff62fb3812d
Pierre-Antoine Delsart <delsart@in2p3.fr>
2014-03-25 16:21:11 -0400

    avoid FPE (svn r589241)

--- JetSubStructureMomentTools/Root/AngularityTool.cxx
+++ JetSubStructureMomentTools/Root/AngularityTool.cxx
@@ -38,7 +38,9 @@ double AngularityTool::angularity(vector<fastjet::PseudoJet> &constit_pseudojets
   for(unsigned int iConstit=0; iConstit < constit_pseudojets.size(); iConstit++) {
     TLorentzVector tclus = TLorentzVector(constit_pseudojets[iConstit].px(),constit_pseudojets[iConstit].py(),constit_pseudojets[iConstit].pz(),constit_pseudojets[iConstit].e());
     double theta_i = jet_p4.Angle(tclus.Vect());
-    double e_theta_i_a2 = constit_pseudojets[iConstit].E()*pow(sin(theta_i),m_a2)*pow(1-cos(theta_i),1-m_a2);
+    double sintheta_i = sin(theta_i);
+    if( sintheta_i == 0 ) continue; // avoid FPE
+    double e_theta_i_a2 = constit_pseudojets[iConstit].E()*pow(sintheta_i,m_a2)*pow(1-cos(theta_i),1-m_a2);
     sum_a2 += e_theta_i_a2;
   }
 
